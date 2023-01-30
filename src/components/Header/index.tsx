047518eda2be2bd05a1587ea2dbb9e23ea2e12c8
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-01-30 14:32:20 -0700

    create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AppBar, Button, Toolbar, Typography, Link } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+interface Props {
+    title: string;
+    links: { label: string; path: string }[];
+}
+
+const Header: React.FC<Props> = ({ title, links }) => {
+    return (
+        <AppBar position="static" color="inherit">
+            <Toolbar>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                    <Link key="/" component={RouterLink} to="/" style={{ color: 'primary.main', textDecoration: 'none' }}>
+                        {title}
+                    </Link>
+                </Typography>
+                {links.map((link) => (
+                    <Button
+                        size='small'
+                        sx={{
+                            backgroundColor: "secondary.main",
+                            color: "white",
+                            marginLeft: "1rem",
+                            '&:hover': {
+                                backgroundColor: 'secondary.dark',
+                            }
+                        }}
+                        component={RouterLink} to={link.path}>
+                        {link.label}
+                    </Button>
+
+                    //   <Link key={link.path} component={RouterLink} to={link.path} style={{ color: 'primary.main', marginLeft: 16, textDecoration: 'none' }}>
+                    //     {link.label}
+                    //   </Link>
+                ))}
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
