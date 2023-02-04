69b201a87647e82aaf39da39d32ae7a8d177be9c
Gary Wu <42899499+garywu2@users.noreply.github.com>
2023-02-04 15:02:01 -0700

    Combining models

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -33,13 +33,92 @@ const userSchema = new mongoose.Schema({
         type: Date,
         required: true,
     },
-    profile: {
-        type: { type: mongoose.SchemaTypes.ObjectID, ref: "Profile" },
+    avatar: {
+        data: String,
+        required: false,
+    },
+    theme: {
+        type: String,
         required: true,
     },
-    coachingProfile: {
-        type: { type: mongoose.SchemaTypes.ObjectID, ref: "CoachingProfile" },
+    payerID: {
+        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "User" }],
+        required: true,
+    },
+    location: {
+        type: String,
         required: false,
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    privacySettings: {
+        type: { type: mongoose.SchemaTypes.ObjectID, ref: "PrivacySetting" },
+        required: true,
+    },
+    dateLastUpdated: {
+        type: Date,
+        required: true
+    },
+    isPremium: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    premiumExpiryDate: {
+        type: Date,
+        required: false
+    },
+    participatingSimulator: {
+        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "Simulator" }],
+        required: true,
+        default: []
+    },
+    badges: {
+        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "UserBadges" }],
+        required: true,
+        default: []
+    },
+    coaches: {
+        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "Coaches" }],
+        required: true,
+        default: []
+    },
+    premiumPaymentHistory: {
+        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "PremiumPaymentHistory" }],
+        required: true,
+        default: []
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    image: {
+        data: String,
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: Number,
+        required: true,
+    },
+    reviews: {
+        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "Review" }],
+        required: true,
+        default: []
+    },
+    clients: {
+        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "Client" }],
+        required: true,
+        default: []
     },
 });
 
