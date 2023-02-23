cab3dc97bde7761b4b44cb883836b2a3d74aee91
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-02-23 16:28:18 -0700

    add faq page

---
+++ src/containers/FaqPage/index.tsx
@@ -0,0 +1,95 @@
+import {
+    Box,
+    Button,
+    Container,
+    Grid,
+    Typography,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails
+} from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import { Link } from 'react-router-dom'
+import { width } from '@mui/system';
+
+
+interface Faq {
+    title: string;
+    description: string;
+}
+
+const faqs: Faq[] = [
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { title: 'Sample FAQ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+];
+
+const FaqPage = () => {
+    return (
+        <div>
+            <Container
+                sx={{
+                    backgroundColor: 'primary.main',
+                    minHeight: '100vh',
+                    minWidth: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: '0 0 3rem 3rem',
+                    boxShadow: '0 4px 15px -6px black',
+                    marginBottom: '1rem',
+                    padding: '5rem',
+                    paddingBottom: '2rem'
+                }}
+            >
+                <Box
+                    display={"flex"}
+                    sx={{
+                        backgroundColor: 'white',
+                        borderRadius: "1rem",
+                        width: "100%",
+                        margin: "3rem",
+                        paddingBottom: "3rem"
+                    }}
+                ><Grid container>
+                        <Grid xs={2}></Grid>
+                        <Grid xs={8}>
+                            <Typography variant="h3" align="center" fontWeight={400} padding={"2rem"}>
+                                Frequently Asked Questions
+                            </Typography>
+                            {faqs.map((faq, index) => (
+                                <Accordion>
+                                    <AccordionSummary
+                                        expandIcon={<ExpandMoreIcon />}
+                                        aria-controls={"panel" + index + "-content"}
+                                        id={"panel" + index + "-header"}
+                                    >
+                                        <Typography>{faq.title}</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <Typography>
+                                            {faq.description}
+                                        </Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                            ))}
+                        </Grid>
+                        <Grid xs={2}></Grid>
+                    </Grid></Box>
+
+            </Container>
+        </div>
+    )
+}
+
+export default FaqPage
