5688de18afcf5241f398b2ee927e0d006dcc92a7
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-02-24 03:36:26 -0700

    create chart

---
+++ src/components/Chart/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer, CartesianGrid } from 'recharts';
+import Title from '../Title';
+
+export default function Chart(data: any) {
+    const theme = useTheme();
+    console.log(data)
+    return (
+        <React.Fragment>
+            <Title>Today</Title>
+            <ResponsiveContainer>
+                <LineChart
+                    data={data}
+                    margin={{
+                        top: 16,
+                        right: 16,
+                        bottom: 0,
+                        left: 24,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis
+                        dataKey="date"
+                        stroke={theme.palette.text.secondary}
+                        style={theme.typography.body2}
+                    />
+                    <YAxis
+                        stroke={theme.palette.text.secondary}
+                        style={theme.typography.body2}
+                    >
+                        <Label
+                            angle={270}
+                            position="left"
+                            style={{
+                                textAnchor: 'middle',
+                                fill: theme.palette.text.primary,
+                                ...theme.typography.body1,
+                            }}
+                        >
+                            Value ($)
+                        </Label>
+                    </YAxis>
+                    <Line
+                        isAnimationActive={false}
+                        type="monotone"
+                        dataKey="price"
+                        stroke={theme.palette.primary.main}
+                        dot={false}
+                    />
+                </LineChart>
+            </ResponsiveContainer>
+        </React.Fragment>
+    );
+}
