0ab9269abf0579ff573c2ac1bdf9238bcc6e8357
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-02-24 01:15:45 -0700

    make login page responsive

--- src/containers/LoginPage/index.tsx
+++ src/containers/LoginPage/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, MenuItem, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
 import { Link } from 'react-router-dom';
 import axios from "axios";
 import { useContext, useState } from "react";
@@ -36,15 +36,15 @@ const LoginPage = () => {
         loginState.username,
         loginState.password
       )
-      .then((servResult: any) => {
-        console.log(servResult);
-        setError("");
-        setLoginState(defaultValues);
-        navigate("/profile");
-      })
-      .catch((e: any) => {
-        setError(e.response.data.msg);
-      });
+        .then((servResult: any) => {
+          console.log(servResult);
+          setError("");
+          setLoginState(defaultValues);
+          navigate("/profile");
+        })
+        .catch((e: any) => {
+          setError(e.response.data.msg);
+        });
     } else {
       setError("One of the required fields is missing!");
     }
@@ -53,75 +53,92 @@ const LoginPage = () => {
   return (
     <div>
       <Container
-        sx={{ backgroundColor: "#7e57c2", minHeight: "100vh", minWidth: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        sx={{
+          backgroundColor: "primary.main",
+          minHeight: "100vh",
+          minWidth: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: '0 0 3rem 3rem',
+          boxShadow: '0 4px 15px -6px black',
+          marginBottom: '1rem',
+          paddingTop: '5rem',
+          paddingBottom: '2rem'
+        }}>
         {!user ? (
           <Box
             component='form'
             display={"flex"}
-            flexDirection='column'
-            alignItems={'center'}
-            justifyContent="center"
-            textAlign={'center'}
-            minWidth="50%"
-            maxWidth="70%"
-            margin="0 auto"
-            padding="2rem"
             sx={{
               backgroundColor: 'white',
-              borderRadius: "1rem"
+              borderRadius: "1rem",
+              width: "100%",
+              paddingBottom: "3rem",
+              margin: { sm: "0rem", lg: "3rem" },
             }}
             noValidate
             autoComplete='off'
           >
-            <Typography variant="h3" align="center" fontWeight={400}>
-              LOG IN
-            </Typography>
-            <div>
-              <TextField
-                required
-                id='login-username'
-                label='Username'
-                name='username'
-                margin="normal"
-                sx={{
-                  width: '100%'
-                }}
-                color="primary"
-                value={loginState.username}
-                onChange={handleChange}
-              />
-              <TextField
-                required
-                id='login-password'
-                label='Password'
-                type='password'
-                name='password'
-                margin="normal"
-                sx={{
-                  width: '100%'
-                }}
-                color="primary"
-                value={loginState.password}
-                onChange={handleChange}
-              />
-            </div>
-            {error && <Typography color='red'>{error}</Typography>}
-            <Button
-              size='medium'
-              sx={{
-                backgroundColor: "secondary.main",
-                color: "white",
-                marginY: "1rem",
-                width: "20%",
-                '&:hover': {
-                  backgroundColor: 'secondary.dark',
-                }
-              }}
-              onClick={handleSumbit}
-            >
-              Log In
-            </Button>
-            <Link to="/register">Create an account</Link>
+            <Grid container>
+              <Grid xs={2}></Grid>
+              <Grid xs={8} sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: 'center',
+                justifyContent: "center",
+                textAlign: "center"
+              }}>
+                <Typography variant="h3" align="center" fontWeight={400} padding={"2rem 0"}>
+                  Log In
+                </Typography>
+                <TextField
+                  required
+                  id='login-username'
+                  label='Username'
+                  name='username'
+                  margin="normal"
+                  sx={{
+                    width: '100%'
+                  }}
+                  color="primary"
+                  value={loginState.username}
+                  onChange={handleChange}
+                />
+                <TextField
+                  required
+                  id='login-password'
+                  label='Password'
+                  type='password'
+                  name='password'
+                  margin="normal"
+                  sx={{
+                    width: '100%'
+                  }}
+                  color="primary"
+                  value={loginState.password}
+                  onChange={handleChange}
+                />
+                {error && <Typography color='red'>{error}</Typography>}
+                <Button
+                  size='medium'
+                  sx={{
+                    backgroundColor: "secondary.main",
+                    color: "white",
+                    marginY: "1rem",
+                    width: "7rem",
+                    '&:hover': {
+                      backgroundColor: 'secondary.dark',
+                    }
+                  }}
+                  onClick={handleSumbit}
+                >
+                  Log In
+                </Button>
+                <Link to="/register">Create an account</Link>
+              </Grid>
+              <Grid xs={2}></Grid>
+            </Grid>
           </Box>
         ) : (
           <Navigate to='/'></Navigate>
