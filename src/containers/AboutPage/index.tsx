96a102059687dc8882216853c1eccaaab81780b4
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-03-12 01:28:24 -0700

    style page

--- src/containers/AboutPage/index.tsx
+++ src/containers/AboutPage/index.tsx
@@ -12,33 +12,39 @@ import TeamPhoto from '../../images/photos/team-photo.jpg'
 interface Bio {
   name: string
   description: string
+  title: string
 }
 
 const bios: Bio[] = [
   {
     name: 'Haseeb Khan',
+    title: 'Software Engineer',
     description:
       'Hello! I enjoy travelling, photography, bouldering, and spontaneous adventures with friends and family. The potential for creating entrepreneurial products and solutions excites me greatly.',
   },
   {
-    name: 'Brooke Mitchell',
+    name: 'Gary Wu',
+    title: 'Software Engineer',
     description:
-      "Hi, I'm Brooke. Ever since I was a kid running my own lemonade stands, I have been fascinated with the idea of entrepreneurial work and self-employment. I am a creative individual and I believe this helps me come up with innovative business ideas and creative solutions to potential problems. I love using Finberry to refine my investment skills and to compete against my fellow founders!",
+      "Hey there! I'm Gary, I have a lot of experience in back-end development and infrastructure. I've worked with React, Express, and various AWS platforms in internships and projects",
   },
   {
-    name: 'Gary Wu',
+    name: 'Brooke Mitchell',
+    title: 'Electrical Engineer',
     description:
-      'I am in my fourth year of a Software Engineering degree. Through previous internship experiences and projects, I’ve had experience with back-end development and the infrastructure side of the application. The primary languages I worked with include React, Express and a variety of different AWS infrastructures.',
+      "I'm Brooke, a creative individual who's had a passion for entrepreneurship since childhood. Finberry helps me refine my investment skills and compete with my fellow founders.",
   },
   {
     name: 'Pin Hong Long',
+    title: 'Software Engineer',
     description:
-      'Hello I am Pin Hong Long, or simply Daniel. I have been fascinated with technology from very early on, this led to my early interest in computers and programming. This is evident by my strong interest in anything technology-related like video games, applications, and websites. With my interest in those fields, I believe it will be very helpful for Finberry.',
+      "Hi, I'm Pin Hong Long, but you can call me Daniel. I've always been interested in computers, programming, and technology-related areas such as video games, apps, and websites.",
   },
   {
     name: 'Muhammad Tariq',
+    title: 'Software Engineer',
     description:
-      'Hi I’m Muhammad Hassan Tariq. I have always been fascinated with computers and different technologies and recently crypto and fintech which led to the idea of our capstone project. I have also always believed in the entrepreneurial spirit and taking meaningful ideas to truly impact the world. Outside of school or work I like playing competitive games whether that be video games, sports, or board games and I also like travelling and spending time with friends and family.',
+      "I'm Muhammad, passionate about tech, crypto, and fintech. I believe in entrepreneurship, meaningful ideas, and love playing competitive games, traveling, and spending time with loved ones.",
   },
 ]
 
@@ -64,15 +70,16 @@ const AboutPage = () => {
           display={'flex'}
           sx={{
             backgroundColor: 'white',
+            alignItems: 'center',
+            justifyContent: 'center',
             borderRadius: '1rem',
             width: '100%',
             margin: { sm: '0rem', lg: '3rem' },
-            paddingBottom: '3rem',
           }}
         >
-          <Grid container>
-            <Grid xs={2}></Grid>
-            <Grid xs={8}>
+          <Grid container sx={{ margin: '2rem' }}>
+            <Grid xs={1}></Grid>
+            <Grid xs={10}>
               <Typography
                 variant='h3'
                 align='center'
@@ -93,10 +100,31 @@ const AboutPage = () => {
                 your own money.
               </Typography>
             </Grid>
-            <Grid xs={2}></Grid>
-            <Container>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
+            <Grid xs={1}></Grid>
+            <Grid item xs={12}>
+              <Card
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  marginBottom: '1rem',
+                }}
+              >
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <CardMedia
+                    component='img'
+                    image={TeamPhoto}
+                    alt='random'
+                    sx={{
+                      borderRadius: '1rem',
+                    }}
+                  />
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid container spacing={2}>
+              {bios.map((bio, index) => (
+                <Grid item key={index} xs={12} sm={12} md={2.4}>
                   <Card
                     sx={{
                       height: '100%',
@@ -108,54 +136,31 @@ const AboutPage = () => {
                     <CardContent sx={{ flexGrow: 1 }}>
                       <CardMedia
                         component='img'
-                        image={TeamPhoto}
+                        image='https://source.unsplash.com/random'
                         alt='random'
                         sx={{
+                          height: '15rem',
                           width: '100%',
                           borderRadius: '1rem',
                           marginBottom: '1rem',
                         }}
                       />
+                      <Typography gutterBottom variant='h5' fontWeight={'bold'}>
+                        {bio.name}
+                      </Typography>
+                      <Typography
+                        gutterBottom
+                        variant='subtitle1'
+                        fontWeight={'bold'}
+                      >
+                        {bio.title}
+                      </Typography>
+                      <Typography>{bio.description}</Typography>
                     </CardContent>
                   </Card>
                 </Grid>
-                {bios.map((bio, index) => (
-                  <Grid item key={index} xs={12} sm={6} md={3}>
-                    <Card
-                      sx={{
-                        height: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                      }}
-                    >
-                      <CardContent sx={{ flexGrow: 1 }}>
-                        <CardMedia
-                          component='img'
-                          image='https://source.unsplash.com/random'
-                          alt='random'
-                          sx={{
-                            height: '15rem',
-                            width: '100%',
-                            borderRadius: '1rem',
-                            marginBottom: '1rem',
-                          }}
-                        />
-                        <Typography
-                          gutterBottom
-                          variant='h5'
-                          align='center'
-                          component='h2'
-                        >
-                          {bio.name}
-                        </Typography>
-                        <Typography>{bio.description}</Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
+              ))}
+            </Grid>
           </Grid>
         </Box>
       </Container>
