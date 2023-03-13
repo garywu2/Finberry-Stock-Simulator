607d5c4b1816c2d4610da70f7014c276264ac475
Gary Wu <42899499+garywu2@users.noreply.github.com>
2023-03-13 04:59:29 -0600

    going crazy

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
 const express         =   require("express"),
       mongoose        =   require("mongoose");
 
-require("../src/models/CoachingProfile");
-require("../src/models/User");
-require("../src/models/Article");
-require("../src/models/Simulator");
-require("../src/models/SimulatorEnrollment");
-require("../src/models/Holding");
-require("../src/models/TradeTransaction");
-require("../src/models/Review");
-require("../src/models/CoachingSession");
+require("./src/models/CoachingProfile");
+require("./src/models/User");
+require("./src/models/Article");
+require("./src/models/Simulator");
+require("./src/models/SimulatorEnrollment");
+require("./src/models/Holding");
+require("./src/models/TradeTransaction");
+require("./src/models/Review");
+require("./src/models/CoachingSession");
 
 const DB = process.env.DB || "mongodb://localhost/finberry";
 mongoose.connect(DB, () => {
   console.log("Database Connected");
 });
           
-const Accounts        =   require("../src/routes/Account"),
+const Accounts        =   require("./src/routes/Account"),
       // User            =   require("./routes/Account/user"),
       // CoachingProfile =   require("./routes/Account/coachingProfile"),
-      Educational     =   require("../src/routes/Educational"),
-      Game            =   require("../src/routes/Game"),
-      Payment         =   require("../src/routes/Payment"),
-      Stock           =   require("../src/routes/Stock"),
+      Educational     =   require("./src/routes/Educational"),
+      Game            =   require("./src/routes/Game"),
+      Payment         =   require("./src/routes/Payment"),
+      Stock           =   require("./src/routes/Stock"),
       cors            =   require("cors");
 
 // import dailyRefresh from "./stock"
