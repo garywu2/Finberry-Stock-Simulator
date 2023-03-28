5bcec9db7f6cd503d2ba4e607ba893070d2fe8c0
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-03-27 22:56:08 -0600

    update avatars that brooke gave

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,132 +1,139 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: { // Each user is unique (From firebase).
-        type: String,
-        required: true,
-        unique: true
+  email: {
+    // Each user is unique (From firebase).
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    // Each user has an unique username.
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  preferredName: {
+    type: String,
+    required: false,
+  },
+  phoneNumber: {
+    type: String,
+    required: false,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: false,
+  },
+  permissionLevel: {
+    // Let 0 be base permission, 1 be moderator, 2 be administrator, and 3 be master authority, the order may be re-arranged as needed.
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  premiumTier: {
+    // Let 1 basic plan (or plus plan), 2 be Enterprise Plan, the order may be re-arranged as needed.
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  // Below are user profile changes
+  avatar: {
+    type: String,
+    required: false,
+    default: '../../images/avatars/av5.png', // Image link: OUR DB like league avatar
+  },
+  theme: {
+    type: Number,
+    required: true,
+    default: 0, // Number coded: 0 = default, etc
+  },
+  location: {
+    type: String,
+    required: true,
+    default: ' ', // Location, technically not required but we can have empty location for simplicity
+  },
+  bio: {
+    type: String,
+    required: true,
+    default: ' ', // User bio, technically not required but we can have empty location for simplicity
+  },
+  premiumExpiryDate: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+  privacySettings: {
+    personalProfile: {
+      type: Boolean,
+      required: true,
+      default: true,
     },
-    username: { // Each user has an unique username.
-        type: String,
-        required: true,
-        unique: true
+    simulator: {
+      type: Boolean,
+      required: true,
+      default: true,
     },
-    firstName: {
-        type: String,
-        required: true,
+    leaderboard: {
+      type: Boolean,
+      required: true,
+      default: true,
     },
-    lastName: {
-        type: String,
-        required: true,
+    badges: {
+      type: Boolean,
+      required: true,
+      default: true,
     },
-    preferredName: {
-        type: String,
-        required: false,
-    },
-    phoneNumber: {
-        type: String,
-        required: false,
-    },
-    dateOfBirth: {
-        type: Date,
-        required: false,
-    },
-    permissionLevel: { // Let 0 be base permission, 1 be moderator, 2 be administrator, and 3 be master authority, the order may be re-arranged as needed.
-        type: Number,
-        required: true,
-        default: 0
-    },
-    premiumTier: { // Let 1 basic plan (or plus plan), 2 be Enterprise Plan, the order may be re-arranged as needed.
-        type: Number,
-        required: true,
-        default: 0
-    },
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now,
-    },
-    // Below are user profile changes
-    avatar: {
-        type: String,
-        required: false,
-        default: "../../images/avatars/icons8-badger-100.png", // Image link: OUR DB like league avatar
-    },
-    theme: { 
-        type: Number,
-        required: true,
-        default: 0, // Number coded: 0 = default, etc
-    },
-    location: {
-        type: String,
-        required: true,
-        default: " ", // Location, technically not required but we can have empty location for simplicity
-    },
-    bio: {
-        type: String,
-        required: true,
-        default: " ", // User bio, technically not required but we can have empty location for simplicity
-    },
-    premiumExpiryDate: {
-        type: Date,
-        required: false,
-        default: null,
-    },
-    privacySettings: { 
-        personalProfile: {
-            type: Boolean,
-            required: true,
-            default: true
-        },
-        simulator: {
-            type: Boolean,
-            required: true,
-            default: true
-        },
-        leaderboard: {
-            type: Boolean,
-            required: true,
-            default: true
-        },
-        badges: {
-            type: Boolean,
-            required: true,
-            default: true
-        }
-    },
-    simulatorEnrollments: { // Does not dirrectly link to the simulator, it links to a proxy class called SimulatorEnrollment
-        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "SimulatorEnrollment" }],
-        required: true,
-        default: []
-    },
-    badges: { // Not implemented yet
-        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "UserBadge" }],
-        required: true,
-        default: []
-    },
-    coachingCoachs: {
-        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "CoachingCoach" }],
-        required: true,
-        default: []
-    },
-    premiumPaymentHistory: { // Not implemented yet
-        type: [{ type: mongoose.SchemaTypes.ObjectID, ref: "PaymentHistory" }],
-        required: true,
-        default: []
-    },
-    // A user may now have a list of coaching profiles.
-    coachingProfiles: {
-        type: [{ type: mongoose.Schema.Types.ObjectId, ref: "CoachingProfile" }],
-        required: true,
-        default: []
-    },
+  },
+  simulatorEnrollments: {
+    // Does not dirrectly link to the simulator, it links to a proxy class called SimulatorEnrollment
+    type: [{ type: mongoose.SchemaTypes.ObjectID, ref: 'SimulatorEnrollment' }],
+    required: true,
+    default: [],
+  },
+  badges: {
+    // Not implemented yet
+    type: [{ type: mongoose.SchemaTypes.ObjectID, ref: 'UserBadge' }],
+    required: true,
+    default: [],
+  },
+  coachingCoachs: {
+    type: [{ type: mongoose.SchemaTypes.ObjectID, ref: 'CoachingCoach' }],
+    required: true,
+    default: [],
+  },
+  premiumPaymentHistory: {
+    // Not implemented yet
+    type: [{ type: mongoose.SchemaTypes.ObjectID, ref: 'PaymentHistory' }],
+    required: true,
+    default: [],
+  },
+  // A user may now have a list of coaching profiles.
+  coachingProfiles: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CoachingProfile' }],
+    required: true,
+    default: [],
+  },
 
-    // Last updated date
-    dateLastUpdated: {
-        type: Date,
-        required: true,
-        default: Date.now,
-    },
-});
+  // Last updated date
+  dateLastUpdated: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+})
 
 module.exports = mongoose.model("User", userSchema);
--- src/containers/ProfilePage/index.tsx
+++ src/containers/ProfilePage/index.tsx
@@ -8,141 +8,134 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { deepOrange } from '@mui/material/colors';
 import { Link, useParams } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles'
 
-const Av1 = require("../../images/avatars/icons8-badger-100.png");
-const Av2 = require("../../images/avatars/icons8-bear-100.png");
-const Av3 = require("../../images/avatars/icons8-leopard-100.png");
-const Av4 = require("../../images/avatars/icons8-lion-100.png");
-const Av5 = require("../../images/avatars/icons8-monkey-face-100.png");
-const Av6 = require("../../images/avatars/icons8-panda-100.png");
-const Av7 = require("../../images/avatars/icons8-tiger-face-100.png");
-const Av8 = require("../../images/avatars/icons8-wolf-100.png");
+const Av1 = require('../../images/avatars/av1.png')
+const Av2 = require('../../images/avatars/av2.png')
+const Av3 = require('../../images/avatars/av3.png')
+const Av4 = require('../../images/avatars/av4.png')
+const Av5 = require('../../images/avatars/av5.png')
 
 const route =
   process.env.REACT_APP_FINBERRY_DEVELOPMENT === 'true'
     ? 'http://localhost:5000/'
     : 'https://finberry-stock-simulator-server.vercel.app/'
-var currImg: any;
+var currImg: any
 
 const ProfilePage = () => {
-    const { user } = useContext(UserContext);
-    const [userItem, setUserItem] = React.useState<any>([]);
-    const [open, setOpen] = React.useState(false);
-    const [open2, setOpen2] = React.useState(false);
-    const [bioText, setBioText] = useState("");
-    const { email } = useParams();
+  const { user } = useContext(UserContext)
+  const [userItem, setUserItem] = React.useState<any>([])
+  const [open, setOpen] = React.useState(false)
+  const [open2, setOpen2] = React.useState(false)
+  const [bioText, setBioText] = useState('')
+  const { email } = useParams()
+  const theme = useTheme()
 
-    const avatars = [
-      { img: Av1, string :"../../images/avatars/icons8-badger-100.png"},
-      { img: Av2, string :"../../images/avatars/icons8-bear-100.png"},
-      { img: Av3, string :"../../images/avatars/icons8-leopard-100.png"},
-      { img: Av4, string :"../../images/avatars/icons8-lion-100.png"},
-      { img: Av5, string :"../../images/avatars/icons8-monkey-face-100.png"},
-      { img: Av6, string :"../../images/avatars/icons8-panda-100.png"},
-      { img: Av7, string :"../../images/avatars/icons8-tiger-face-100.png"},
-      { img: Av8, string :"../../images/avatars/icons8-wolf-100.png"}
-    ];
+  const avatars = [
+    { img: Av1, string: '../../images/avatars/av1.png' },
+    { img: Av2, string: '../../images/avatars/av2.png' },
+    { img: Av3, string: '../../images/avatars/av3.png' },
+    { img: Av4, string: '../../images/avatars/av4.png' },
+    { img: Av5, string: '../../images/avatars/av5.png' },
+  ]
 
-    const setCurrImg = (imgStr: any) => {
-      for(var i = 0; i < avatars.length; i++) {
-        if(avatars[i].string == imgStr) {
-          currImg = avatars[i].img;
-        }
+  const setCurrImg = (imgStr: any) => {
+    for (var i = 0; i < avatars.length; i++) {
+      if (avatars[i].string == imgStr) {
+        currImg = avatars[i].img
       }
-    };
-
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-
-    const handleClickOpen2 = () => {
-      setOpen2(true);
-    };
-
-    const handleClose = (event: any) => {
-      setOpen(false);
-      setCurrImg(String(event.currentTarget.value));
-      
-      axios({
-        method: 'put',
-        url:
-          route +
-          'account/user/email/' +
-          String(user.email),
-        headers: {},
-        data: {
-          avatar: String(event.currentTarget.value)
-        }
-      });
-    };
-
-    const handleClose2 = (event: any) => {
-      setOpen2(false);
-    };
-
-    const handleBioSubmit = (event: any) => {
-      axios({
-        method: 'put',
-        url:
-          route +
-          'account/user/email/' +
-          String(user.email),
-        headers: {},
-        data: {
-          bio: bioText
-        }
-      }).then((result: any) => {
-        axios.get(route + 'account/user', {
-          params: {
-            email: String(user.email)
-          }
-        }).then((response) => {
-          setUserItem(response.data[0]);
-        })
-      });
-
-      setOpen2(false);
-    };
-
-    const handleBioTextChange = (event: any) => {
-      setBioText(event.target.value)
     }
+  }
 
-    React.useEffect(() => {
-      axios.get(route + 'account/user', {
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClickOpen2 = () => {
+    setOpen2(true)
+  }
+
+  const handleClose = (event: any) => {
+    setOpen(false)
+    setCurrImg(String(event.currentTarget.value))
+
+    axios({
+      method: 'put',
+      url: route + 'account/user/email/' + String(user.email),
+      headers: {},
+      data: {
+        avatar: String(event.currentTarget.value),
+      },
+    })
+  }
+
+  const handleClose2 = (event: any) => {
+    setOpen2(false)
+  }
+
+  const handleBioSubmit = (event: any) => {
+    axios({
+      method: 'put',
+      url: route + 'account/user/email/' + String(user.email),
+      headers: {},
+      data: {
+        bio: bioText,
+      },
+    }).then((result: any) => {
+      axios
+        .get(route + 'account/user', {
+          params: {
+            email: String(user.email),
+          },
+        })
+        .then((response) => {
+          setUserItem(response.data[0])
+        })
+    })
+
+    setOpen2(false)
+  }
+
+  const handleBioTextChange = (event: any) => {
+    setBioText(event.target.value)
+  }
+
+  React.useEffect(() => {
+    axios
+      .get(route + 'account/user', {
         params: {
-          email: String(user.email)
-        }
-      }).then((response) => {
-        setUserItem(response.data[0]);
-        if(response.data[0]){
-          setCurrImg(response.data[0].avatar);
+          email: String(user.email),
+        },
+      })
+      .then((response) => {
+        setUserItem(response.data[0])
+        if (response.data[0]) {
+          setCurrImg(response.data[0].avatar)
         }
       })
-    }, [email]);
+  }, [email])
 
-    return (
-      <Container
-        sx={{
-          backgroundColor: 'primary.main',
-          minHeight: '100vh',
-          minWidth: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: '3rem',
-          boxShadow: '0 4px 15px -6px black',
-          marginBottom: '1rem',
-          paddingTop: '5rem',
-          paddingBottom: '2rem',
-          overflow: 'auto',
-        }}
-      >
-        <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
-          { userItem ? (
-            <Grid container spacing={3}>
+  return (
+    <Container
+      sx={{
+        backgroundColor: 'primary.main',
+        minHeight: '100vh',
+        minWidth: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '3rem',
+        boxShadow: '0 4px 15px -6px black',
+        marginBottom: '1rem',
+        paddingTop: '5rem',
+        paddingBottom: '2rem',
+        overflow: 'auto',
+      }}
+    >
+      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+        {userItem ? (
+          <Grid container spacing={3}>
             <Grid item xs={12} md={4} lg={3}>
               <Paper
                 sx={{
@@ -151,15 +144,16 @@ const ProfilePage = () => {
                   flexDirection: 'column',
                   height: 240,
                 }}
-              > 
-
-                           
+              >
                 <Avatar
-                    sx={{ bgcolor: deepOrange[500], width: 120, height: 120}}
-                    alt={userItem.firstName}
-                    src={currImg}
-                  >
-                </Avatar>
+                  sx={{
+                    bgcolor: theme.palette.secondary.main,
+                    width: 120,
+                    height: 120,
+                  }}
+                  alt={userItem.firstName}
+                  src={currImg}
+                ></Avatar>
 
                 <Typography variant='body1' align='center' fontWeight={400}>
                   {userItem.username}
@@ -168,30 +162,36 @@ const ProfilePage = () => {
                 <Typography variant='body1' align='center' fontWeight={400}>
                   {userItem.firstName} {userItem.lastName}
                 </Typography>
-                {user.email === email &&
-                <Button variant="outlined" onClick={handleClickOpen}>
-                  Change Avatar
-                </Button>
-               } 
+                {user.email === email && (
+                  <Button variant='outlined' onClick={handleClickOpen}>
+                    Change Avatar
+                  </Button>
+                )}
                 <Dialog open={open} onClose={handleClose}>
                   <DialogTitle>Pick an Avatar</DialogTitle>
                   <DialogContent>
-                      {avatars && (
-                        <DialogContentText>
+                    {avatars && (
+                      <DialogContentText>
                         {avatars.map((avatar) => (
-                                <Button variant="outlined" value={avatar.string} startIcon={<Avatar
-                                                                    sx={{ bgcolor: deepOrange[500], width: 120, height: 120}}
-                                                                    alt={userItem.firstName}
-                                                                    src={avatar.img}
-                                                                  >
-                                                                </Avatar>}
-                                                          
-                                                                onClick={handleClose}
-                                >
-                                </Button>
-                              ))}
-                        </DialogContentText>
-                      )}
+                          <Button
+                            variant='outlined'
+                            value={avatar.string}
+                            startIcon={
+                              <Avatar
+                                sx={{
+                                  bgcolor: theme.palette.secondary.main,
+                                  width: 120,
+                                  height: 120,
+                                }}
+                                alt={userItem.firstName}
+                                src={avatar.img}
+                              ></Avatar>
+                            }
+                            onClick={handleClose}
+                          ></Button>
+                        ))}
+                      </DialogContentText>
+                    )}
                   </DialogContent>
                   <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
@@ -214,21 +214,21 @@ const ProfilePage = () => {
                 <Typography variant='body1' align='left' fontWeight={400}>
                   {userItem.bio}
                 </Typography>
-                {user.email === email &&
-                <Button variant="outlined" onClick={handleClickOpen2}>
-                  Edit Bio
-                </Button>
-                }
+                {user.email === email && (
+                  <Button variant='outlined' onClick={handleClickOpen2}>
+                    Edit Bio
+                  </Button>
+                )}
                 <Dialog open={open2} onClose={handleClose2}>
                   <DialogTitle>Enter your new bio</DialogTitle>
                   <DialogContent>
                     <DialogContentText>
                       <TextField
-                        id="outlined-multiline-static"
-                        label=""
+                        id='outlined-multiline-static'
+                        label=''
                         multiline
                         rows={4}
-                        defaultValue="I love DogeCoin"
+                        defaultValue='I love DogeCoin'
                         value={bioText}
                         onChange={handleBioTextChange}
                       />
@@ -242,31 +242,26 @@ const ProfilePage = () => {
               </Paper>
             </Grid>
             <Grid item xs={12}>
-              <Paper
-                sx={{ p: 2, display: 'flex', flexDirection: 'column' }}
-              >
+              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
                 <Typography variant='h4' align='left' fontWeight={400}>
-                  Badges: 
+                  Badges:
                 </Typography>
               </Paper>
             </Grid>
             <Grid item xs={12}>
-              <Paper
-                sx={{ p: 2, display: 'flex', flexDirection: 'column' }}
-              >
+              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
                 <Typography variant='h4' align='left' fontWeight={400}>
-                  Coaching Sessions: 
+                  Coaching Sessions:
                 </Typography>
               </Paper>
             </Grid>
           </Grid>
-          ) : (
-            <></>
-          )}  
-          
-        </Container>
+        ) : (
+          <></>
+        )}
       </Container>
-    )
+    </Container>
+  )
 }
 
 export default ProfilePage;
