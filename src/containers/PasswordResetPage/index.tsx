81781cae977ad86b26c5f3c85ab9a23a140e1fd1
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-03-28 05:12:02 -0600

    capitalize Reset Password

--- src/containers/PasswordResetPage/index.tsx
+++ src/containers/PasswordResetPage/index.tsx
@@ -26,81 +26,95 @@ const LoginPage = () => {
     <div>
       <Container
         sx={{
-          backgroundColor: "primary.main",
-          minHeight: "100vh",
-          minWidth: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          backgroundColor: 'primary.main',
+          minHeight: '100vh',
+          minWidth: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
           borderRadius: '0 0 3rem 3rem',
           boxShadow: '0 4px 15px -6px black',
           marginBottom: '1rem',
           paddingTop: '5rem',
-          paddingBottom: '2rem'
-        }}>
-          <Box
-            component='form'
-            display={"flex"}
-            sx={{
-              backgroundColor: 'white',
-              borderRadius: "1rem",
-              width: { xs: "100%", sm: "100%", md: "75%", lg: "50%" },
-              paddingBottom: "3rem",
-              margin: { sm: "0rem", lg: "3rem" },
-            }}
-            noValidate
-            autoComplete='off'
-          >
-            <Grid container>
-              <Grid xs={2}></Grid>
-              <Grid xs={8} sx={{
-                display: "flex",
-                flexDirection: "column",
+          paddingBottom: '2rem',
+        }}
+      >
+        <Box
+          component='form'
+          display={'flex'}
+          sx={{
+            backgroundColor: 'white',
+            borderRadius: '1rem',
+            width: { xs: '100%', sm: '100%', md: '75%', lg: '50%' },
+            paddingBottom: '3rem',
+            margin: { sm: '0rem', lg: '3rem' },
+          }}
+          noValidate
+          autoComplete='off'
+        >
+          <Grid container>
+            <Grid xs={2}></Grid>
+            <Grid
+              xs={8}
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
                 alignItems: 'center',
-                justifyContent: "center",
-                textAlign: "center"
-              }}>
-                <Typography variant="h3" align="center" fontWeight={400} padding={"2rem 0"}>
-                  Reset password
-                </Typography>
-                <TextField
-                  required
-                  type='email'
-                  id='email'
-                  label='Email'
-                  name='email'
-                  margin="normal"
-                  sx={{
-                    width: '100%'
-                  }}
-                  color="primary"
-                  defaultValue={userEmail}
-                  onChange={handleChange}
-                />
-                {error && <Typography color='red'>{error}</Typography>}
-                <Button
-                  size='medium'
-                  fullWidth
-                  sx={{
-                    backgroundColor: "secondary.main",
-                    color: "white",
-                    marginY: "1rem",
-                    '&:hover': {
-                      backgroundColor: 'secondary.dark',
-                    }
-                  }}
-                  onClick={resetPassword}
-                >
-                  Reset Password
-                </Button>
-                <Link style={{ fontFamily: 'Fredoka', margin: "10px" }} to="/login">Return to login page</Link>
-                {emailSent && <Typography color='green'>{emailSent}</Typography>}
-              </Grid>
-              <Grid xs={2}></Grid>
+                justifyContent: 'center',
+                textAlign: 'center',
+              }}
+            >
+              <Typography
+                variant='h3'
+                align='center'
+                fontWeight={400}
+                padding={'2rem 0'}
+              >
+                Reset Password
+              </Typography>
+              <TextField
+                required
+                type='email'
+                id='email'
+                label='Email'
+                name='email'
+                margin='normal'
+                sx={{
+                  width: '100%',
+                }}
+                color='primary'
+                defaultValue={userEmail}
+                onChange={handleChange}
+              />
+              {error && <Typography color='red'>{error}</Typography>}
+              <Button
+                size='medium'
+                fullWidth
+                sx={{
+                  backgroundColor: 'secondary.main',
+                  color: 'white',
+                  marginY: '1rem',
+                  '&:hover': {
+                    backgroundColor: 'secondary.dark',
+                  },
+                }}
+                onClick={resetPassword}
+              >
+                Reset Password
+              </Button>
+              <Link
+                style={{ fontFamily: 'Fredoka', margin: '10px' }}
+                to='/login'
+              >
+                Return to login page
+              </Link>
+              {emailSent && <Typography color='green'>{emailSent}</Typography>}
             </Grid>
-          </Box>
+            <Grid xs={2}></Grid>
+          </Grid>
+        </Box>
       </Container>
     </div>
-  );
+  )
 };
 export default LoginPage;
