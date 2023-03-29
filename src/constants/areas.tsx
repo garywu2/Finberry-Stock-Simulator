9d4dd55197c7711b814fce08ae29f279ac681b86
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2023-03-29 03:36:59 -0600

    create spinner component

---
+++ src/constants/areas.tsx
@@ -0,0 +1,9 @@
+export const areas = {
+  coachCatalogue: 'coach-catalogue-area',
+  coachPortalBioSubmit: 'coach-portal-bio-submit-area',
+  coachPortalUserImage: 'coach-portal-user-image-area',
+  coachPortalUserInfo: 'coach-portal-user-info-area',
+  profileUserInfo: 'profile-user-info-area',
+  profileUserBioSubmit: 'profile-user-bio-submit-area',
+  simulatorPortfolioUserInfo: 'simulator-portfolio-user-info-area',
+}
